eb829e3c4215dc6ea0cfc4aab9e5f06fdfa8c45a
Bob Lam <boblam.ceo@gmail.com>
2021-03-20 09:39:48 +0800

    create horizontal list and add humaaans

--- news-2/App.js
+++ news-2/App.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Image, SafeAreaView, FlatList, View } from "react-native";
+import CardComp from "./components/card";
+import Header from "./components/header";
 
 export default function App() {
+  const news = [
+    { title: "Covid cases exeed 1900!", author: "John Doe", date: new Date() },
+    {
+      title: "Hong Kong protesters stop doing stuff",
+      author: "John Doe",
+      date: new Date(),
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Bruv</Text>
+      <Header></Header>
+      <FlatList
+        horizontal={true}
+        data={news}
+        renderItem={({ item: { title, date, author } }) => {
+          return (
+            <CardComp title={title} date={date} author={author}></CardComp>
+          );
+        }}
+        keyExtractor={(item, index) => `${index}`}
+        showsHorizontalScrollIndicator={false}
+      ></FlatList>
+      <View style={styles.horizontal}>
+        <Image
+          source={require("./assets/sitting.png")}
+          style={styles.image}
+        ></Image>
+        <Image
+          source={require("./assets/sitting2.png")}
+          style={styles.image}
+        ></Image>
+      </View>
     </View>
   );
 }
@@ -13,7 +44,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "center",
+    padding: 20,
+    paddingTop: 40,
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
   },
 });
---
+++ news-2/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Header = () => {
+  return (
+    <View>
+      <Text>heh</Text>
+    </View>
+  );
+};
+
+export default Header;
