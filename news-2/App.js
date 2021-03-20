be3114fe38ea0a1043f7cf29a8fa9b30b8501795
Bob Lam <boblam.ceo@gmail.com>
2021-03-20 14:13:33 +0800

    create header

--- news-2/App.js
+++ news-2/App.js
@@ -13,41 +13,42 @@ export default function App() {
     },
   ];
   return (
-    <View style={styles.container}>
+    <View>
       <Header></Header>
-      <FlatList
-        horizontal={true}
-        data={news}
-        renderItem={({ item: { title, date, author } }) => {
-          return (
-            <CardComp title={title} date={date} author={author}></CardComp>
-          );
-        }}
-        keyExtractor={(item, index) => `${index}`}
-        showsHorizontalScrollIndicator={false}
-      ></FlatList>
-      <View style={styles.horizontal}>
-        <Image
-          source={require("./assets/sitting.png")}
-          style={styles.image}
-        ></Image>
-        <Image
-          source={require("./assets/sitting2.png")}
-          style={styles.image}
-        ></Image>
+      <View style={styles.wrapper}>
+        <FlatList
+          horizontal={true}
+          data={news}
+          renderItem={({ item: { title, date, author } }) => {
+            return (
+              <CardComp title={title} date={date} author={author}></CardComp>
+            );
+          }}
+          keyExtractor={(item, index) => `${index}`}
+          showsHorizontalScrollIndicator={false}
+        ></FlatList>
+        <View style={styles.horizontal}>
+          <Image
+            source={require("./assets/sitting.png")}
+            style={styles.image}
+          ></Image>
+          <Image
+            source={require("./assets/sitting2.png")}
+            style={styles.image}
+          ></Image>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  wrapper: {
+    height: "100%",
     backgroundColor: "#fff",
     alignItems: "flex-start",
     justifyContent: "center",
-    padding: 20,
-    paddingTop: 40,
+    paddingBottom: 170,
   },
   horizontal: {
     flexDirection: "row",
--- news-2/components/header.js
+++ news-2/components/header.js
@@ -1,12 +1,54 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { IconButton, Colors } from "react-native-paper";
 
 const Header = () => {
   return (
-    <View>
-      <Text>heh</Text>
+    <View style={[styles.header, styles.row]}>
+      <View style={[styles.stuff, styles.row]}>
+        <View style={[styles.left, styles.row]}>
+          <IconButton
+            icon="menu"
+            size={30}
+            onPress={() => console.log("Pressed")}
+          />
+        </View>
+        <View style={[styles.right, styles.row]}>
+          <Text style={styles.title}>The News App</Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#43D26C",
+    width: "100%",
+    height: 80,
+    alignItems: "center",
+  },
+  stuff: {
+    // backgroundColor: "red",
+    width: 900,
+    marginTop: 30,
+    padding: 10,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  left: {
+    width: "12%",
+    alignItems: "center",
+  },
+  right: {
+    width: "36%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+  },
+});
 
 export default Header;
